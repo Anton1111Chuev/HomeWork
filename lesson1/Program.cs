4f58fa9f9d89766156db1829c533b81da029b117
Anton <Afrser@gmail.com>
2022-08-22 21:21:50 +0300

    Второй урок

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,56 +1,56 @@
 ﻿
-// Задача 2
-//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.Clear();
+// // Задача 2
+// //Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Clear();
 
-Console.WriteLine ("Введите первое число ");
-int number1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите первое число ");
+// int number1 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine ("Введите второе число ");
-int number2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите второе число ");
+// int number2 = int.Parse(Console.ReadLine()!);
 
-if (number1 > number2) {
-    Console.WriteLine("Первое число больше");
-}
-else if (number2 > number1) {
-    Console.WriteLine("Второе число больше");
-}
-else {
-    Console.WriteLine("Числа равны");
-}
+// if (number1 > number2) {
+//     Console.WriteLine("Первое число больше");
+// }
+// else if (number2 > number1) {
+//     Console.WriteLine("Второе число больше");
+// }
+// else {
+//     Console.WriteLine("Числа равны");
+// }
 
-//Задача 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// //Задача 4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine ("Введите первое число ");
-number1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите первое число ");
+// number1 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine ("Введите второе число ");
-number2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите второе число ");
+// number2 = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine ("Введите третье число ");
-int number3 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите третье число ");
+// int number3 = int.Parse(Console.ReadLine()!);
 
-int max = number1;
+// int max = number1;
 
-if (max < number2) {
-     max = number2;
-}
-if (max < number3) {
-    max = number3;
-}
-Console.WriteLine ("Максимум " + max);
+// if (max < number2) {
+//      max = number2;
+// }
+// if (max < number3) {
+//     max = number3;
+// }
+// Console.WriteLine ("Максимум " + max);
 
-//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-Console.WriteLine ("Введите  число ");
-int number = int.Parse(Console.ReadLine()!);
+// Console.WriteLine ("Введите  число ");
+// int number = int.Parse(Console.ReadLine()!);
 
-if (number % 2 == 0 ){
-   Console.WriteLine ("Число четное");
-}
-else {
-    Console.WriteLine ("Число  не четное");
-}
+// if (number % 2 == 0 ){
+//    Console.WriteLine ("Число четное");
+// }
+// else {
+//     Console.WriteLine ("Число  не четное");
+// }
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
---
+++ lesson2/Program.cs
@@ -0,0 +1,35 @@
+﻿void Write (string text = "Введите число:"){
+    System.Console.WriteLine(text);  
+}
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Write();
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine((number%100 - number%10) / 10 );
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Write();
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < 100) {
+   Write ("У введенного числа нет третей цифры");
+}
+else {
+    System.Console.WriteLine( (number1%1000  - number1%100)/100);
+}
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Write("Введите число дня недели");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayOfWeek == 6 || dayOfWeek == 7) {
+    Write("Это выходной");
+}
+else{
+    Write("Это не выходной");
+}
+
+
